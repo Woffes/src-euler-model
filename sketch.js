9017caa605ef4be6081f0aa10de191a1ec8eaa12
Woffes <squidg4ce@gmail.com>
2023-05-16 14:11:07 +0200

    c n b

---
+++ sketch.js
@@ -0,0 +1,24 @@
+let dy
+let c = 299792
+let rs = 25
+let r = 1000 //y værdi
+let t = 0 //x værdi
+let yn
+
+function setup() {
+  dy=-c*(1-rs/r)*sqrt(rs/r)
+  createCanvas(700, 700)
+  return dy
+}
+
+function draw() {
+  background(220);
+  antalPunkter()
+}
+
+function antalPunkter(){
+  let punkter=[]
+  let i
+  for(let i = 0; 0 < i, i++;)
+  punkter[i]=[]
+}
