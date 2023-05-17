e38c3192d75e1cf6d02c5d322e2213bcfd55ebf2
Woffes <squidg4ce@gmail.com>
2023-05-17 11:04:36 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,8 @@
 let rs = 25; //Sorte huls radius
 let c = 299792; //Lysets hastighed
-let h = 0.001; // Skridtlængden
-let r = 1000; // Startpunkt fra det sorte hul
-let end = 1; // Slutpunkt
-
+let h = 0.001; //Skridtlængden
+let r = 1000; //Startpunkt fra det sorte hul
+let end = 1; //Slutpunkt
 
 function setup() {
   createCanvas(700, 700)
@@ -11,10 +10,10 @@ function setup() {
 
 function draw() {
   background(220);
-  eulerMethode()
+  eulerMethode(frameCount)
 }
 
-  function eulerMethode() {
+  function eulerMethode(frame) {
     for (let t = 0; t <= end; t += h) {
       const derivative = -c * (1 - rs / r) * Math.sqrt(rs / r);
       r += derivative * h;
